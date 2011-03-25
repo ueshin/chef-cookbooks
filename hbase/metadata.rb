def352792b0378e2cf3768cc20045c36cbf1e22f
Takuya UESHIN <ueshin@happy-camper.st>
2011-03-26 02:01:33 +0900

    cookbook for hbase-0.90.1+8 (CDH3B4)
    Signed-off-by: Takuya UESHIN <ueshin@happy-camper.st>

---
+++ hbase/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Happy-Camper Street"
+maintainer_email "ueshin@happy-camper.st"
+license          "Apache 2.0"
+description      "Installs/Configures hbase"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
---
+++ hbase/recipes/default.rb
@@ -0,0 +1,74 @@
+#
+# Cookbook Name:: hbase
+# Recipe:: default
+#
+# Copyright 2011, Happy-Camper Street
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "hadoop"
+
+group "hbase" do
+  gid 204
+end
+
+user "hbase" do
+  uid "204"
+  gid "hbase"
+  comment "HBase"
+  home "/var/run/hbase"
+  shell "/sbin/nologin"
+end
+
+hbasetmpdir = node[:hbase][:tmp][:dir]
+
+directory File.dirname(hbasetmpdir) do
+  mode "0755"
+  recursive true
+  not_if do
+    File.exists?(File.dirname(hbasetmpdir))
+  end
+end
+
+directory hbasetmpdir do
+  owner "hbase"
+  group "hbase"
+  mode "0777"
+end
+
+package "hadoop-hbase" do
+  version node[:hbase][:version]
+end
+
+cookbook_file "/etc/hbase/conf/hadoop-metrics.properties" do
+  source "hadoop-metrics.properties"
+  mode "0644"
+end
+
+template "/etc/hbase/conf/hbase-site.xml" do
+  source "hbase-site.xml.erb"
+  mode "0644"
+
+  variables( :namenode     => search(:node, 'role:NameNode')[0],
+             :namenodeport => node[:hadoop][:core][:namenodeport],
+             :hbasetmpdir  => hbasetmpdir,
+             :zookeepers   => search(:node, 'role:ZooKeeper').sort_by { |zk| zk[:hostname] } )
+end
+
+template "/etc/hbase/conf/regionservers" do
+  source "regionservers.erb"
+  mode "0644"
+
+  variables( :regionservers => search(:node, 'role:RegionServer').sort_by { |rs| rs[:hostname] } )
+end
